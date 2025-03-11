2c6ba68d4d861f7b25694f91b2daaa1cf68b2292
Dot9tee2 <mianmehroze@gmail.com>
2025-03-11 07:07:02 +0500

    tawk removed

--- index.php
+++ index.php
@@ -199,21 +199,6 @@
   </section>
   <?php include 'footer.php'; ?>
 
-  <!--Start of Tawk.to Script-->
-  <script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    (function() {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/67cc0ccdd508e8190fc7b10e/1ilqh416i';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin', '*');
-      s0.parentNode.insertBefore(s1, s0);
-    })();
-  </script>
-  <!--End of Tawk.to Script-->
   <script type="module" src="js/main.js"></script>
 </body>
 
